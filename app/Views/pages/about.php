02942449c2db51c1d595e713a37bb604fca45ac2
Bakhtiar <itsproinc@gmail.com>
2020-11-10 09:49:59 +0700

    Copied from ci4app

---
+++ app/Views/pages/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam purus sit amet luctus. Neque sodales ut etiam sit amet nisl purus in. Semper auctor neque vitae tempus quam pellentesque nec nam. Commodo elit at imperdiet dui accumsan sit. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Urna molestie at elementum eu facilisis sed. Erat pellentesque adipiscing commodo elit. Tortor consequat id porta nibh venenatis cras. Sagittis id consectetur purus ut faucibus pulvinar. Aliquam nulla facilisi cras fermentum odio eu feugiat. Vulputate mi sit amet mauris commodo quis. Enim lobortis scelerisque fermentum dui faucibus in ornare. Eget egestas purus viverra accumsan in. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Nulla aliquet enim tortor at. Elit pellentesque habitant morbi tristique.</p>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
